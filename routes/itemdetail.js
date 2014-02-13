a91917fc25e5163c31d63b67d50b450499aedfac
Elizabeth Chaddock <echaddoc@ucsd.edu>
2014-02-13 14:21:10 -0800

    item detail page works omg yay

--- routes/itemdetail.js
+++ routes/itemdetail.js
@@ -1,9 +1,31 @@
-var itemData = require('../data.json');
+var data =require('../data.json')['group'];
 
 exports.viewItem = function(req, res){
 	var name = req.params.name;
-	console.log("The item name is: " + name);
+    var price, size, condition, color;
+
+  for(var i = 0,len=data.length; i<len; i++) //0 or 1
+  {
+    for(var j = 0, len2 = data[i]['item'].length; j<len2; j++)
+    {
+      if(data[i]['item'][j]['name'] === name)
+      {
+        price = data[i]['item'][j]['price'];
+        size =  data[i]['item'][j]['size'];
+        condition = data[i]['item'][j]['condition'];
+        color = data[i]['item'][j]['color'];
+        imageURL = data[i]['item'][j]['imageURL'];
+      }
+    }
+
+  }
+
 	res.render('itemdetail', {
-		'itemName' : name
+    'itemName' : name,
+    'price' : price,
+    'size' : size,
+    'condition' : condition,
+    'color': color,
+    'imageURL' : imageURL
 	});
 };
